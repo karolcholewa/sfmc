40622526b21e919d8032b64044ca2f91afb9d020
karolcholewa <karol.ch@gmail.com>
2021-10-04 17:55:18 +0200

    Update retrieveContentArea.js

--- retrieveContentArea.js
+++ retrieveContentArea.js
@@ -1,7 +1,7 @@
 <script runat="server">
   
         Platform.Load("core", "1");
-        var req = ContentAreaObj.Retrieve({Property:"ID",SimpleOperator:"equals",Value:14653510});
+        var req = ContentAreaObj.Retrieve({Property:"ID",SimpleOperator:"equals",Value:13814211});
         Write(Stringify(req[0].Name));
 
 </script>
